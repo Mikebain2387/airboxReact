be731eb3febf54f7f9131bb7d49de2a7b995baa1
Michael Bain <150988106+Mikebain2387@users.noreply.github.com>
2024-10-30 16:06:16 -0500

    Update body.js

--- src/components/body.js
+++ src/components/body.js
@@ -1,6 +1,8 @@
 import React from "react"
 import gears from '../images/gears-removebg-preview (1).png'
 import domain from '../images/domain-removebg-preview.png'
+import cloud from '../images/cloud-removebg-preview.png'
+import phone from '../images/phone-removebg-preview.png'
 function Body(){
  return
 }
@@ -22,16 +24,16 @@ const boxData = [
   info:  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque."
  },
  {
-  image:
-  title:
-  subtitle:
-  info:  
+  image: cloud,
+  title: "FTP",
+  subtitle: "Service",
+  info:  " Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque."
  },
  {
-  image:
-  title:
-  subtitle:
-  info:  
+  image: phone,
+  title: "Direct",
+  subtitle: "Support",
+  info:  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque."
  }
 ]
 
